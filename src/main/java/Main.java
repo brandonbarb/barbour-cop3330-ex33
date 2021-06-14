29077069cd96687f08d42204c64e33f1fd6fa58f
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-13 21:40:03 -0400

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,18 @@
  *  Copyright 2021 Brandon Barbour
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String[] answers = {"Yes", "No", "Maybe", "Ask again later."};
+        Random number = new Random();
+        System.out.print("What's your question?\n> ");
+        Scanner input = new Scanner(System.in);
+        String question = input.next();
 
+        int i = number.nextInt(4);
+        System.out.printf("%s", answers[i]);
     }
 }
